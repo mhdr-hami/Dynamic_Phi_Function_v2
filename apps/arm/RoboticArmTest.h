64ccb653a815bbff2361ccad490fa0db07ec7f81
Mohammadreza Hami <mhdr.hami@gmail.com>
2024-11-16 10:54:37 -0700

    arm commit

--- apps/arm/RoboticArmTest.h
+++ apps/arm/RoboticArmTest.h
@@ -11,12 +11,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * HOG is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with HOG; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -33,7 +33,7 @@ void MyKeyHandler(unsigned long windowID, tKeyboardModifier, char key);
 
 
 int MyCLHandler(char *argument[], int maxNumArgs);
-bool MyClickHandler(unsigned long windowID, int viewport, int x, int y, point3d loc, tButtonType, tMouseEventType);
+bool MyClickHandler(unsigned long windowID, int x, int y, point3d loc, tButtonType, tMouseEventType);
 void InstallHandlers();
 
 void GenerateHeuristic();
